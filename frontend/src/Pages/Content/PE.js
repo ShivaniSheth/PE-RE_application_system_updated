c9e114d6f0f7e7008d7e24382386b3439891190a
Ashu-Soni <ashu07101998@gmail.com>
2022-04-09 18:32:20 +0530

    added search bar

--- frontend/src/Pages/Content/PE.js
+++ frontend/src/Pages/Content/PE.js
@@ -1,65 +1,111 @@
-import { Col, Row, Table } from 'antd';
-import React, { Component } from 'react';
+import { Col, Row, Table, Button, Input } from 'antd';
+import Layout, { Content, Header } from 'antd/lib/layout/layout';
+import React, { Component, useState } from 'react';
+
+const { Search } = Input;
+let cols = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+      title: 'Professor',
+      dataIndex: 'professor',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+  },
+  {
+      title: 'Slots Available',
+      dataIndex: 'slots',
+  },
+  {
+    title: 'Apply',
+    dataIndex: 'apply',
+  },
+];
+
+let projs = [
+  {
+    key: '1',
+    name: 'PE/RE Application System',
+    professor: "ABC",
+    description: "Hello world",
+    slots: 10,
+    apply: <Button type='primary'>apply</Button>
+  },
+  {
+      key: '2',
+      name: 'PE/RE Application System',
+      professor: "ABC",
+      description: "Hello world",
+      slots: 5,
+      apply: <Button type='primary'>apply</Button>
+  },
+  {
+      key: '3',
+      name: 'Health care management',
+      professor: "sdfsdf",
+      description: "Hello world",
+      slots: 4,
+      apply: <Button type='primary'>apply</Button>
+  },
+];
 
 export default class PE extends Component {
-    render(){
-    let columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          sorter: (a, b) => a.name.length - b.name.length,
-          sortDirections: ['descend'],
-        },
-        {
-            title: 'Professor',
-            dataIndex: 'professor',
-            sorter: (a, b) => a.name.length - b.name.length,
-            sortDirections: ['descend'],
-        },
-        {
-          title: 'Description',
-          dataIndex: 'description',
-        },
-        {
-            title: 'Slots Available',
-            dataIndex: 'slots',
-            defaultSortOrder: 'descend',
-            sorter: (a, b) => a.age - b.age,
-        },
-      ];
-      
-    let data = [
-        {
-          key: '1',
-          name: 'PE/RE Application System',
-          professor: "ABC",
-          description: "Hello world",
-          slots: 10,
-        },
-        {
-            key: '2',
-            name: 'PE/RE Application System',
-            professor: "ABC",
-            description: "Hello world",
-            slots: 5,
-        },
-        {
-            key: '3',
-            name: 'Health care management',
-            professor: "sdfsdf",
-            description: "Hello world",
-            slots: 4,
-        },
-      ];
+  state = {
+    columns: cols,
+    projects: projs,
+    filtered: projs,
+    count: "",
+  };
+
+  onSearch = (value) => {
+    let data = this.state.projects;
     
-        return(
-            <Row justify='center' align='middle'>
-                <Col span={24}>
-                    <div>
-                    <Table columns={columns} dataSource={data} />
-                    </div>
-                </Col>
-            </Row>
-        )
+    if (value.length === 0) {
+      this.setState({filtered: data})
+      return;
     }
+    let searchLower = value.toLowerCase();
+    let filtered_projects = data.filter((item) => {
+      console.log(item.professor)
+      if (item.professor.toLowerCase().includes(searchLower)) {
+        return true;
+      }
+      return false;
+    });
+    this.setState({filtered: filtered_projects})
+  };
+
+  onSearchChange = (e) => {
+    if (e.target.value.length === 0) {
+      this.onSearch("");
+    }
+  };
+
+  render(){   
+    const { count, columns, projects, filtered } = this.state;
+    console.log(projects)
+    console.log(filtered)
+
+    return(
+      <Layout>
+        <Header style={{backgroundColor: 'whitesmoke'}}>
+          <Search
+            placeholder="Search by Professor Name"
+            allowClear
+            style={{ width: "80%", marginTop: "15px", marginLeft: "8%" }}
+            onSearch={this.onSearch}
+            name="professorSearch"
+            onChange={this.onSearchChange}
+          />
+        </Header>
+        <Content>
+          <Table columns={columns} dataSource={filtered} />
+        </Content>
+      </Layout>
+    )
+  }
 }
--- frontend/src/Pages/Content/RE.js
+++ frontend/src/Pages/Content/RE.js
@@ -1,54 +1,103 @@
-import { Col, Row, Table, Breadcrumb } from 'antd';
-import Layout from 'antd/lib/layout/layout';
-import React, { Component } from 'react';
+import { Col, Row, Table, Button, Input } from 'antd';
+import Layout, { Content, Header } from 'antd/lib/layout/layout';
+import React, { Component, useState } from 'react';
 
-const { Header, Content, Sider } = Layout;
+const { Search } = Input;
+let cols = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+      title: 'Professor',
+      dataIndex: 'professor',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+  },
+  {
+      title: 'Slots Available',
+      dataIndex: 'slots',
+  },
+  {
+    title: 'Apply',
+    dataIndex: 'apply',
+  },
+];
+
+let projs = [
+  {
+    key: '1',
+    name: 'PE/RE Application System',
+    professor: "ABC",
+    description: "Hello world",
+    slots: 10,
+    apply: <Button type='primary'>apply</Button>
+  },
+  {
+      key: '2',
+      name: 'Health care management',
+      professor: "sdfsdf",
+      description: "Hello world",
+      slots: 4,
+      apply: <Button type='primary'>apply</Button>
+  },
+];
 
 export default class RE extends Component {
-    render(){
-    let columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          sorter: (a, b) => a.name.length - b.name.length,
-          sortDirections: ['descend'],
-        },
-        {
-            title: 'Professor',
-            dataIndex: 'professor',
-            sorter: (a, b) => a.name.length - b.name.length,
-            sortDirections: ['descend'],
-        },
-        {
-          title: 'Description',
-          dataIndex: 'description',
-        },
-        {
-            title: 'Slots Available',
-            dataIndex: 'slots',
-            defaultSortOrder: 'descend',
-            sorter: (a, b) => a.age - b.age,
-        },
-      ];
-      
-    let data = [
-        {
-          key: '1',
-          name: 'PE/RE Application System',
-          professor: "ABC",
-          description: "Hello world",
-          slots: 10,
-        },
-      ];
+  state = {
+    columns: cols,
+    projects: projs,
+    filtered: projs,
+    count: "",
+  };
+
+  onSearch = (value) => {
+    let data = this.state.projects;
     
-        return(
-            <Row justify='center' align='middle'>
-                <Col span={24}>
-                    <div>
-                    <Table columns={columns} dataSource={data} />
-                    </div>
-                </Col>
-            </Row>
-        )
+    if (value.length === 0) {
+      this.setState({filtered: data})
+      return;
     }
+    let searchLower = value.toLowerCase();
+    let filtered_projects = data.filter((item) => {
+      console.log(item.professor)
+      if (item.professor.toLowerCase().includes(searchLower)) {
+        return true;
+      }
+      return false;
+    });
+    this.setState({filtered: filtered_projects})
+  };
+
+  onSearchChange = (e) => {
+    if (e.target.value.length === 0) {
+      this.onSearch("");
+    }
+  };
+
+  render(){   
+    const { count, columns, projects, filtered } = this.state;
+    console.log(projects)
+    console.log(filtered)
+
+    return(
+      <Layout>
+        <Header style={{backgroundColor: 'whitesmoke'}}>
+          <Search
+            placeholder="Search by Professor Name"
+            allowClear
+            style={{ width: "80%", marginTop: "15px", marginLeft: "8%" }}
+            onSearch={this.onSearch}
+            name="professorSearch"
+            onChange={this.onSearchChange}
+          />
+        </Header>
+        <Content>
+          <Table columns={columns} dataSource={filtered} />
+        </Content>
+      </Layout>
+    )
+  }
 }
